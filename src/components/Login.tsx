0653ee410a2111657002c2ef9ddd646d812d14cd
facundoiglesias9 <facundoiglesias9@gmail.com>
2025-12-01 21:50:31 -0300

    Update login label

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -156,7 +156,7 @@ export default function Login() {
                     {isRegistering && (
                         <div>
                             <label className="block text-sm font-medium text-gray-300 mb-2 ml-1">
-                                Nombre del Emprendimiento
+                                Nombre
                             </label>
                             <input
                                 type="text"
